31bbeefcf2a8df944df0b667dee0d2c63a699281
Lavysh Alexander <LavyshAlexander@gmail.com>
2021-09-22 20:03:24 +0300

    Rewritten tests for selection sort with common sorting helpers.

--- test/algorithms/sorting/selection/selectionSort.test.ts
+++ test/algorithms/sorting/selection/selectionSort.test.ts
@@ -1,32 +1,21 @@
-import { selectionSort } from '../../../../src/algorithms/sorting'
 import 'mocha'
-import { expect } from 'chai'
+import { selectionSort } from '../../../../src/algorithms/sorting'
+import { testUnsortedArray, testEmptyArray, testEmptyArrayRef } from '../common'
 
 
-describe( 'selection sort', () => {
-	it( 'should return sorted array', () => {
-		const unsorted = [ 1,2,9,8,7,5,0 ]
-		const sorted   = [ 0,1,2,5,7,8,9 ]
+describe( 'bubble sort', () => {
+	it(
+		'should return sorted array',
+		testUnsortedArray( selectionSort )
+	)
 
-		const result = selectionSort( unsorted )
+	it(
+		'should return empty array if in input was empty array',
+		testEmptyArray( selectionSort )
+	)
 
-		expect( result ).to.deep.equal( sorted )
-	} )
-
-	it( 'should return empty array if in input was empty array', () => {
-		const emptyArray: number[] = []
-
-		const result = selectionSort( emptyArray )
-
-		expect( result ).to.be.empty
-	} )
-
-	it( 'should return empty array if in input was empty array but by different ref', () => {
-		const emptyArray: number[] = []
-
-		const result = selectionSort( emptyArray )
-
-		expect( result ).to.be.empty
-		expect( result ).to.not.equal( emptyArray )
-	} )
+	it(
+		'should return empty array if in input was empty array but by different ref',
+		testEmptyArrayRef( selectionSort )
+	)
 })
