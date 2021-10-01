979d598fec3b53cdf49da210b7e84f8f9c84ebce
Lavysh Alexander <LavyshAlexander@gmail.com>
2021-10-01 20:21:30 +0300

    Added straight forward merge sort implementation.

--- src/algorithms/sorting/merge/mergeSort.ts
+++ src/algorithms/sorting/merge/mergeSort.ts
@@ -1,3 +1,40 @@
 export function mergeSort( array: number[] ): number[] {
-	return [ 0 ]
+	const result = mergeSortByRecursion( [ ...array ] )
+
+	return result
+}
+
+function mergeSortByRecursion( array: number[] ): number[] {
+	if ( array.length <= 1 ) {
+		return array
+	}
+
+	const splitIndex = Math.ceil( array.length / 2 )
+	// TODO: implement without using additional memory
+	let leftPart = array.slice( 0, splitIndex )
+	let rightPart = array.slice( splitIndex )
+
+	leftPart = mergeSortByRecursion( leftPart )
+	rightPart = mergeSortByRecursion( rightPart )
+
+	const result = new Array<number>()
+	let i = 0
+	let j = 0
+
+	while ( i < leftPart.length ) {
+		while( j < rightPart.length && leftPart[i] > rightPart[j] ) {
+			result.push( rightPart[j] )
+			j++
+		}
+
+		result.push( leftPart[i] )
+		i++
+	}
+
+	while ( j < rightPart.length ) {
+		result.push( rightPart[j] )
+		j++
+	}
+
+	return result
 }
