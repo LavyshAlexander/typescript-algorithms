c52387cf475fe741591a74d852acebe115880bfb
Lavysh Alexander <LavyshAlexander@gmail.com>
2021-10-02 17:24:38 +0300

    Added merge sort implementation without additional memory usage.

--- src/algorithms/sorting/merge/mergeSort.ts
+++ src/algorithms/sorting/merge/mergeSort.ts
@@ -1,21 +1,52 @@
 export function mergeSort( array: number[] ): number[] {
-	const result = mergeSortByRecursion( [ ...array ] )
+	// const result = sortWithAdditionalMemory( [ ...array ] )
+	const result = [ ...array ]
+
+	splitMerge( array, 0, array.length, result )
 
 	return result
 }
 
-function mergeSortByRecursion( array: number[] ): number[] {
+function splitMerge( array: number[], start: number, end: number, result: number[] ): void {
+	if ( end - start <= 1 ) {
+		return
+	}
+
+	const split = Math.floor( (end + start) / 2 )
+	splitMerge( result, start, split, array )
+	splitMerge( result, split, end, array )
+
+	merge( array, start, split, end, result )
+}
+
+function merge( array: number[], start: number, split: number, end: number, result: number[] ): void {
+	let i = start
+	let j = split
+
+	for ( let n = start; n < end; n++ ) {
+		if ( i < split && ( j >= end || array[i] <= array[j] ) ) {
+			result[n] = array[i]
+			i++
+		} else {
+			result[n] = array[j]
+			j++
+		}
+	}
+}
+
+
+
+function sortWithAdditionalMemory( array: number[] ): number[] {
 	if ( array.length <= 1 ) {
 		return array
 	}
 
 	const splitIndex = Math.ceil( array.length / 2 )
-	// TODO: implement without using additional memory
 	let leftPart = array.slice( 0, splitIndex )
 	let rightPart = array.slice( splitIndex )
 
-	leftPart = mergeSortByRecursion( leftPart )
-	rightPart = mergeSortByRecursion( rightPart )
+	leftPart = sortWithAdditionalMemory( leftPart )
+	rightPart = sortWithAdditionalMemory( rightPart )
 
 	const result = new Array<number>()
 	let i = 0
