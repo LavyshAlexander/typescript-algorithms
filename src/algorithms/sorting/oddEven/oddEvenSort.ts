8d6bfbf78fa236a5b471ea5145aab8945dfc289c
Lavysh Alexander <LavyshAlexander@gmail.com>
2021-10-04 16:20:54 +0300

    Added odd-even sort algorithm implementation.

--- src/algorithms/sorting/oddEven/oddEvenSort.ts
+++ src/algorithms/sorting/oddEven/oddEvenSort.ts
@@ -1,5 +1,26 @@
 export function oddEvenSort( array: number[] ): number[] {
-	const result = [ 0 ]
+	const result = [ ...array ]
+
+	let doNextRound = true
+
+	do {
+		doNextRound = false
+
+		for( let i = 0 ; i < result.length - 1 ; i += 2) {
+			if ( result[i] > result[i + 1] ) {
+				[ result[i], result[i + 1] ] = [ result[i + 1], result[i] ]
+				doNextRound = true
+			}
+		}
+
+		for( let i = 1 ; i < result.length - 1 ; i += 2 ) {
+			if ( result[i] > result[i + 1] ) {
+				[ result[i], result[i + 1] ] = [ result[i + 1], result[i] ]
+				doNextRound = true
+			}
+		}
+
+	} while( doNextRound )
 
 	return result
 }
